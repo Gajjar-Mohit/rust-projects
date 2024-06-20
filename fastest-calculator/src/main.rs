083fcf2445eee8dd6a9ec59b76159def2d3b8bba
Mohit Gajjar <mohit@Mohits-Air.bbrouter>
2024-06-20 17:23:29 +0530

    dynamic calculator

--- fastest-calculator/src/main.rs
+++ fastest-calculator/src/main.rs
@@ -1,59 +1,103 @@
-use std::io::{self, Read, Write};
+use std::io::{self, Write};
 fn main() {
     println!("==================================================");
     println!("Welcome to super fast calculator");
     println!("==================================================");
-    print!("Enter the first number: ");
-    let _res = std::io::stdout().flush();
-    let mut temp1 = String::new();
+    println!("1: Operation");
+    println!("2: exit");
+    println!("----------------------------------------------------");
 
-    io::stdin()
-        .read_line(&mut temp1)
-        .expect("Failed to read line");
-    let number1: f32 = temp1.trim().parse().expect("Please enter a number!");
+    let exit_var: bool = false;
+    while exit_var == false {
+        print!("Enter your choice: ");
+        let _res = std::io::stdout().flush().expect("Failed to flush stdout");
 
-    print!("Enter the second number: ");
-    let _res1 = std::io::stdout().flush();
-    let mut temp2 = String::new();
-    io::stdin()
-        .read_line(&mut temp2)
-        .expect("Failed to read line");
+        let mut temp1 = String::new();
 
-    let number2: f32 = temp2.trim().parse().expect("Please enter a number!");
-    println!(
-        "The addition of {} and {} is {}",
-        number1,
-        number2,
-        add(number1, number2)
-    );
-    println!(
-        "The subtraction of {} and {} is {}",
-        number1,
-        number2,
-        sub(number1, number2)
-    );
-    println!(
-        "The multiplication of {} and {} is {}",
-        number1,
-        number2,
-        mul(number1, number2)
-    );
-    println!(
-        "The division of {} and {} is {}",
-        number1,
-        number2,
-        div(number1, number2)
-    );
+        io::stdin()
+            .read_line(&mut temp1)
+            .expect("Failed to read line");
+        let choice: i8 = temp1.trim().parse().unwrap();
+        match choice {
+            1 => operate(),
+            2 => {
+                println!("Exiting the program");
+                break;
+            }
+            _ => println!("Invalid choice"),
+        }
+    }
 }
+
+fn operate() {
+    print!("Input: ");
+    let _res = std::io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut temp = String::new();
+
+    io::stdin()
+        .read_line(&mut temp)
+        .expect("Failed to read line");
+    let input: Vec<&str> = temp.trim().split(" ").collect();
+    let num1: f32 = input[0].parse().unwrap();
+    let num2: f32 = input[2].parse().unwrap();
+    match input[1] {
+        "+" => println!(
+            "Result of addition of {} and {} is {}",
+            num1,
+            num2,
+            add(num1, num2)
+        ),
+        "-" => println!(
+            "Result of substraction of {} and {} is {}",
+            num1,
+            num2,
+            sub(num1, num2)
+        ),
+        "*" => println!(
+            "Result of multiplication of {} and {} is {}",
+            num1,
+            num2,
+            mul(num1, num2)
+        ),
+        "/" => println!(
+            "Result of division of {} and {} is {}",
+            num1,
+            num2,
+            div(num1, num2)
+        ),
+        "%" => println!(
+            "Result of modulo of {} and {} is {}",
+            num1,
+            num2,
+            md(num1, num2)
+        ),
+        "^" => println!(
+            "Result of power of {} and {} is {}",
+            num1,
+            num2,
+            pow(num1, num2)
+        ),
+        _ => println!("Invalid operator"),
+    }
+}
+
 fn add(a: f32, b: f32) -> f32 {
     return a + b;
 }
 fn sub(a: f32, b: f32) -> f32 {
     return a - b;
 }
+
 fn mul(a: f32, b: f32) -> f32 {
-    return a + b;
+    return a * b;
 }
 fn div(a: f32, b: f32) -> f32 {
-    return a + b;
+    return a / b;
+}
+fn md(a: f32, b: f32) -> f32 {
+    return a % b;
+}
+fn pow(a: f32, b: f32) -> f32 {
+    return a.powf(b);
 }
